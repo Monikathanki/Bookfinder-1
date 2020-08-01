97ba2a3ab866883db208d0c664fac3f03d5cd5da
Monikathanki <63766164+Monikathanki@users.noreply.github.com>
2020-08-01 11:39:47 -0700

    Index.js
    new file

--- index.js
+++ index.js
@@ -1,72 +1,90 @@
 'use strict';
 
+
+
+
+
 const GOOGLE_BOOK_SEARCH_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
   
-  function getDataFromGoogleBooksApi(book) {
-    let url = GOOGLE_BOOK_SEARCH_URL + book;
-    const query = {
-      key: 'AIzaSyCXn1s41z5eUTYyq5L2JAc4YZpao_7ephk',
-      q: book,
+function getDataFromGoogleBooksApi(book) {
+  let url = GOOGLE_BOOK_SEARCH_URL + book;
+  const query = {
+    key: 'AIzaSyCXn1s41z5eUTYyq5L2JAc4YZpao_7ephk',
+    q: book,
       
-      limit: 'maxResults',
-      orderBy: 'relevance'
-    };
-    fetch(url)
-    .then(res => res.json()).then(responseJson => displayBooks(responseJson));
-  }
+    limit: 'maxResults',
+    orderBy: 'relevance'
+  };
+  fetch(url)
+  .then(res => {
+    if(!res.ok){
+      throw new Error()
+    }
+    return res.json()
+    })
+    .then(responseJson => displayBooks(responseJson))
+    .catch((e) => {
+      $('#gb-results').html("");
+      $('#gb-results').append(`
+        <div class="channel goog">Google-Books</div>
+      `);
+
+      $('#gb-results').append(`<h2>Sorry, no results!</h2>`);
+    })
+}
 
 
 
 function displayBooks( result ){
+  // console.log(result)
    $('#gb-results').html(" ");
    $('#gb-results').append(`
      <div class="channel goog">Google-Books</div>
   `);
-  if(result){
-    result.items.forEach((item) => {
-      $('#gb-results').append(`
-      <div class="gb-row">
-        <div class="gb-1">
-          <a href="${item.volumeInfo.canonicalVolumeLink}" target="_blank"><img class="gb-thumbs" src="${item.volumeInfo.imageLinks.thumbnail}"></a>
+
+      result.items.forEach((item) => {
+        $('#gb-results').append(`
+        <div class="gb-row">
+          <div class="gb-1">
+            <a href="${item.volumeInfo.canonicalVolumeLink}" target="_blank"><img class="gb-thumbs" src="
+            ${item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : 
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRI_-WvjF1ELri4w-r9umQI0JBiSNS-j1-J4w&usqp=CAU'}"></a>
+          </div>
+          <div class="gb-2">
+            <h2 class="gb-title">Title: ${item.volumeInfo.title}</h2>
+            <h4>Author: ${item.volumeInfo.authors}</h4>
+            <p class="gb-info">${item.volumeInfo.subtitle}</p>
+          </div>
         </div>
-        <div class="gb-2">
-          <h2 class="gb-title">Title: ${item.volumeInfo.title}</h2>
-          <h4>Author: ${item.volumeInfo.authors
-          }</h4>
-         <p class="gb-info">${item.volumeInfo.subtitle}</p>
-       </div>
-      </div>
-    `);
-    });
-  } else {
-     $('#gb-results').append(`<h2>Sorry, no results!</h2>`);
-  }
-
+      `);
+      });
 }
-
-
- 
-
-
-
-
-
-
-
-
 
 
 const WIKI_SEARCH_URL = 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cpageimages&exsentences=3&list=&titles=';
   
 function getDataFromWikiApi(authorName) {
   const url = WIKI_SEARCH_URL + authorName;
-  console.log(url)
+  // console.log(url)
   const queryData = "";
-  return fetch(url, {
+   
+  fetch(url, {
     headers: {
     'Content-Type': 'application/json',
   },
-  }).then(res => res.json()).then(responseJson=> displayAuthor(responseJson));
+  }).then(res => {
+    if(!res.ok) {
+      throw new Error()
+    }
+    return res.json()
+  })
+  .then(responseJson => displayAuthor(responseJson))
+  .catch((e) => {
+    $('#wiki-results').html("");
+    $('#wiki-results').append(`<div class="channel wiki">Author Details</div>`);
+    $('#wiki-results').append(`<p>Try to be more specific!</p>`);
+  });
+  
 }
 
 function displayAuthor(data ){
@@ -74,7 +92,7 @@ function displayAuthor(data ){
   $('#wiki-results').append(`<div class="channel wiki">Author Details</div>`);
 
   let pages = data.query.pages;
-  console.log(pages);
+  // console.log(pages);
    
     for (let id in pages) {
       if(pages[id] == undefined) {
@@ -84,12 +102,12 @@ function displayAuthor(data ){
 
 
       if (pages[id].thumbnail == undefined) {
-        console.log("there is no thumbnail source, its undefined");
+        // console.log("there is no thumbnail source, its undefined");
 
       }
 
       if (pages[id].thumbnail !== undefined) {  
-        $('#wiki-results').append(`<img class="wiki-img" src="{pages[id].thumbnail.source}">`);
+        $('#wiki-results').append(`<img class="wiki-pic" src="${pages[id].thumbnail.source}">`);
       }
 
       if (pages[id].extract == "") {
@@ -102,7 +120,7 @@ function displayAuthor(data ){
       } 
 
       else {
-        console.log("its undefined");
+        // console.log("its undefined");
         $('#wiki-results').append(`<p>Try to be more specific!</p>`);
       } 
 
@@ -114,22 +132,57 @@ function displayAuthor(data ){
 function watchForm() {
   $('#js-form').submit((event) => {
     event.preventDefault();
+
     $('#results').removeClass('hidden');
-    let authorName =  $('.js-query').val();
-    let book = $('.js-query').val();
+    let authorName =  $('.author-input').val();
+    let book = $('.title-input').val();
 
-    getDataFromWikiApi(authorName);
-    getDataFromGoogleBooksApi(book);
-  
-  
+    if(authorName) {
+      getDataFromWikiApi(authorName);
+      $('#gb-results').hide();
+      $('#wiki-results').show()
+    }else if(book) {
+      getDataFromGoogleBooksApi(book);
+      $('#wiki-results').hide();
+      $('#gb-results').show();
+    }else {
+      return
+    }
+    
   });
-
-
-
+}
+function displayInfo() {
+  $('#js-info').on('click', () => {
+  $('#moreInfo').toggle();
+}); 
 
 }
 
-$(watchForm);
+function displayInputs() {
+  $('.author-input').hide()
+  $('input[type="radio"]').on('change',(e)=> {
+    let value = $(e.currentTarget).val()
+    if(value === 'author') {
+      $('.author-input').show()
+       $('.author-input').prop('required', true)
+      $('.title-input').prop('required', false)
+      $('.title-input').hide()
+      $('.title-input').val('')
+    } else {
+      $('.author-input').hide()
+      $('.author-input').val('')
+      $('.title-input').show()
+      $('.title-input').prop('required', true)
+      $('.author-input').prop('required', false)
+    }
+  })
+}
+
+$(
+  watchForm(),
+  displayInputs(),
+  displayInfo()
+);
 
 
 
